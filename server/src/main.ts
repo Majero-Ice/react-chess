cdcf3413013d4c42d268bcd1df6a42faf99f15a1
Maxim <maximnegoitsa55@gmail.com>
2023-11-21 16:39:06 +0100

    commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,9 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT
   app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true
+    origin: [
+      'https://react-chess-production-4f5b.up.railway.app/socket.io/?EIO=4&transport=polling&t=OloXwt2'
+    ],
+    methods: ["GET", "POST","DELETE"],
+    credentials: true,
   });
   await app.listen(PORT);
 }
