f193208a67257b4b635b49ce64cdd11cc73203d4
Maxim <maximnegoitsa55@gmail.com>
2023-11-21 16:35:46 +0100

    commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true
+  });
   await app.listen(PORT);
 }
 bootstrap();
