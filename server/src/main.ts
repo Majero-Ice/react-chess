7ce8c88d4f2765d863f6eae5391b838f7f4f0fa1
Maxim <maximnegoitsa55@gmail.com>
2024-02-25 14:19:25 +0100

    prod-commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: ['https://react-chess-client.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true
   })
